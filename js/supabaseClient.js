934f8752408da9e461aae3faf4ce7568457f2f55
Happytatoes <noaheverestwong@gmail.com>
2025-09-08 15:58:30 -0500

    persist-session

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const SUPABASE_URL = 'https://orgnduftnhgufeiweyoe.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZ25kdWZ0bmhndWZlaXdleW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0MzAzMTIsImV4cCI6MjA2OTAwNjMxMn0.Tdty67UyqoagmxFFhnINdg4zt3YbwwPZJdbPDjnupcI';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,   // saves session in localStorage
+    autoRefreshToken: true, // keeps session alive
+    detectSessionInUrl: true,
+  },
+});
